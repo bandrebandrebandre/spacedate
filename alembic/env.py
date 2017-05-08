098c5eee7a9e14269fd1b1a77730dcc41e1f63fb
bandre <ben@movesystems.com>
2017-05-08 01:24:15 +0100

    Add alembic revision.

--- alembic/env.py
+++ alembic/env.py
@@ -15,7 +15,8 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from spacedate.models.user import BaseModel
+target_metadata = BaseModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
